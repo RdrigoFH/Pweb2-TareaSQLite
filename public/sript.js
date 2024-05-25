58fe480c3e36acc1ab85248c0be4841da7674d92
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-05-25 11:36:54 -0500

    si la conversion de la respuesta del then anterior a json fue exitosa se creara una constante que contendra la lista del html para mostrar las peliculas

--- public/sript.js
+++ public/sript.js
@@ -1,5 +1,9 @@
 document.getElementById('loadMovies').addEventListener('click', () => {
    fetch('/movies')
        .then(response => response.json())
-       
+       .then(data => {
+           const moviesList = document.getElementById('moviesList');
+           moviesList.innerHTML = '';
+       })
+       .catch(error => console.error('Error:', error));
 });
