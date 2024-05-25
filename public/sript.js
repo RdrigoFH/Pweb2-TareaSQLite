241a109c34d22f32df6f7ce67ef587948ddd5817
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-05-25 11:39:38 -0500

    se recorre cada elemetno de movie se crea el elemento li para el html y se asigna los id de la pelicula titulo de las tablas de ibdb.db

--- public/sript.js
+++ public/sript.js
@@ -4,6 +4,11 @@ document.getElementById('loadMovies').addEventListener('click', () => {
        .then(data => {
            const moviesList = document.getElementById('moviesList');
            moviesList.innerHTML = '';
+           data.data.forEach(movie => {
+               const li = document.createElement('li');
+               li.textContent = `${movie.MovieID}: ${movie.Title} (${movie.Year}) - Score: ${movie.Score}, Votes: ${movie.Votes}`;
+               moviesList.appendChild(li);
+           });
        })
        .catch(error => console.error('Error:', error));
 });
